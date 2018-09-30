44dd9bd2ac9dd9ea8165e26155febe749e1952dd
Xinyu Zhao <xinyuzhao23@gmail.com>
2018-09-30 18:27:14 -0400

    map with points

--- script.py
+++ script.py
@@ -15,6 +15,7 @@ class JSONParser:
     def __init__(self):
         self.wd = os.listdir("../yelp")
         self.businesses = []
+        self.location_business_num = Counter()
         self.location_review_num = Counter()
         self.latitudes = []
         self.longitudes = []
@@ -55,8 +56,8 @@ class JSONParser:
         file.close()
         for b in lines:
             d = json.loads(b)
-            if (d["review_count"] >10):
-                self.dict.append(d)
+            # if (d["review_count"] >10):
+            self.dict.append(d)
             # print(d["longitude"], d["latitude"])
         print(self.dict[0].keys())
         print(lines[0])
@@ -64,18 +65,24 @@ class JSONParser:
 
     def count_reviews(self):
         for business in self.businesses:
-            if business['stars'] < 2.5:
+            if business['latitude'] is not None and business['longitude'] is not None:
                 lat = business['latitude']
                 long = business['longitude']
 
-                round_lat = round(lat, 2)
-                round_long = round(long, 2)
+                round_lat = round(lat, 1)
+                round_long = round(long, 1)
                 # loc = Location(round_lat, round_long)
                 current_location = (round_lat, round_long)
 
-                self.latitudes.append(round_lat)
-                self.longitudes.append(round_long)
-                self.location_review_num[current_location] += 1
+                self.location_review_num[current_location] += business['review_count']
+                self.location_business_num[current_location] += 1
+
+        for key in self.location_review_num.keys():
+            self.latitudes.append(key[0])
+            self.longitudes.append(key[1])
+
+        print(len(self.latitudes))
+        print(len(self.longitudes))
 
 
 
@@ -94,12 +101,21 @@ class JSONParser:
 
         return review_densities
 
-    def create_map(self, latitudes, longitudes, review_counts):
-        review_densities = self.create_locations(latitudes, longitudes, review_counts)
-        fig = plt.figure(figsize=(8, 8))
-        m = Basemap(projection='lcc', resolution='h',
-                    lat_0=37.5, lon_0=-119,
-                    width=8E6, height=8E6)
+    def create_review(self, latitudes, longitudes, review_counts):
+        review_densities = []
+
+        for index in range(len(latitudes)):
+            current_location = (latitudes[index], longitudes[index])
+            review_densities.append(review_counts[current_location])
+
+        return review_densities
+
+    def create_map(self, latitudes, longitudes, business_counts, review_counts):
+        location_densities = self.create_locations(latitudes, longitudes, business_counts)
+        review_densities = self.create_review(latitudes, longitudes, review_counts)
+        fig = plt.figure(figsize=(10, 10))
+        m = Basemap(llcrnrlon=-116,llcrnrlat=35,urcrnrlon=-113,urcrnrlat=38,
+        projection='lcc',lat_1=33,lat_2=45,lon_0=-95, resolution='h')
         m.shadedrelief()
         m.drawcoastlines(color='gray')
         m.drawcountries(color='gray')
@@ -108,19 +124,19 @@ class JSONParser:
         # 2. scatter city data, with color reflecting population
         # and size reflecting area
         m.scatter(longitudes, latitudes, latlon=True,
-                  c=np.log10(review_densities), s=10,
-                  cmap='Reds', alpha=0.5)
+                  c=review_densities, s=location_densities,
+                  cmap='cool', alpha=0.2)
 
         # 3. create colorbar and legend
-        plt.colorbar(label=r'$\log_{2}({\rm reviewDensities})$')
-        plt.clim(3, 7)
-
-        # make legend with dummy points
-        for a in [100, 300, 500]:
-            plt.scatter([], [], c='k', alpha=0.5, s=10,
-                        label=str(10) + ' km$^2$')
-        plt.legend(scatterpoints=1, frameon=False,
-                   labelspacing=1, loc='lower left');
+        # plt.colorbar(label=r'$\log_{2}({\rm reviewDensities})$')
+        # plt.clim(3, 7)
+        #
+        # # make legend with dummy points
+        # for a in [100, 300, 500]:
+        #     plt.scatter([], [], c='k', alpha=0.5, s=10,
+        #                 label=str(10) + ' km$^2$')
+        # plt.legend(scatterpoints=1, frameon=False,
+        #            labelspacing=1, loc='lower left');
 
         plt.show()
 
@@ -132,4 +148,4 @@ jp = JSONParser()
 jp.read_business()
 # jp.read_user()
 jp.count_reviews()
-jp.create_map(jp.latitudes, jp.longitudes, jp.location_review_num)
+jp.create_map(jp.latitudes, jp.longitudes, jp.location_business_num, jp.location_review_num)
